3343cf7d49f8cb05caee332c352477fe66ec2bfd
franciscobauce <franciscobauce@gmail.com>
2021-05-07 17:47:24 -0300

    oi

---
+++ ex071_caixa eletronico v. Guanabara.py
@@ -0,0 +1,24 @@
+valor = int(input('Digite o valor do saque:'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'total de cedulas de R${ced} é {totced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 1
+        totced = 0
+    if total < 0:
+        break
+
+
---
+++ ex072_numeros por extenso.py
@@ -0,0 +1,10 @@
+extenso = ('zero','um', 'dois',
+           'três', 'quatro', 'cinco',
+           'seis', 'sete', 'oito', 'nove',
+           'dez', 'onze', 'doze', 'treze' ,
+           'quatorze', 'quinze', 'dezesseis',
+           'dezessete', 'dezoito', 'dezenove', 'vinte')
+numero = (int(input('Digite um número:')))
+print(extenso[numero])
+
+
